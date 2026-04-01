d5f41bd9993bb72488722eaadb323bec5bc0c1fc
dharmishthakothari <dharmishtha.only@gmail.com>
2026-04-01 15:08:20 +0530

    Add files via upload

---
+++ 5_March_Java/FileExample.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class FileExample {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Admin\\Documents\\Java_Backup.txt");
+        System.out.println(file.exists());
+        System.out.println(file.getParent() + "\t" + file.getPath());
+        System.out.println(file.isFile() + "\t" + file.isDirectory());
+    }
+}
